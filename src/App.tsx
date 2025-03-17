22ea061ee5efd270bd2e300e0902ead76fefa95e
MayHyeyeonKim <hkim7963@gmail.com>
2025-03-16 19:14:17 -0700

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router";
+import "./App.css";
+import AppLayout from "./layout/AppLayout";
+import SearchPage from "./pages/SearchPage";
+import HomePage from "./pages/HomePage";
+import SearchWithKeywordPage from "./pages/SearchWithKeywordPage";
+import LibraryPage from "./pages/LibraryPage";
+/**
+0. sidebar
+1. landingPage /
+2. searchPage /search/
+3. searchResultPage /serch/:keyword
+4. playlistDetailPage /palylist/:id
+5. mobileVersion - playlist /playlist
+*/
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="search" element={<SearchPage />} />
+        <Route path="search/:keyword" element={<SearchWithKeywordPage />} />
+        <Route path="playlist/:id" element={<LibraryPage />} />
+        {/* <Route path="playlist" element={<PlaylistPage />} /> */}
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
