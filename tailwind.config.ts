95736a6282b46a5b28863b9823534c94b0f0a3a9
MayHyeyeonKim <hkim7963@gmail.com>
2025-03-16 21:15:17 -0700

    added tailwind setting.. 😱errrrr hellll...

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,11 +4,47 @@ const config: Config = {
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
-        "./src/components/**/*.{js,ts,jsx,tsx}", // 컴포넌트 폴더 포함 (선택 사항)
-        "./src/**/*.{css,html}", // Tailwind가 감지해야 할 추가 파일
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{css,html}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#1ed760",
+                secondary: "#ffffff",
+                background: {
+                    default: "#000000",
+                    paper: "#121212",
+                },
+                text: {
+                    primary: "#ffffff",
+                    secondary: "#b3b3b3",
+                },
+                action: {
+                    hover: "#282828",
+                    active: "#333333",
+                },
+            },
+            fontFamily: {
+                sans: ["Roboto", "Arial", "sans-serif"],
+            },
+            fontSize: {
+                h1: "24px",
+                h2: "1rem",
+                body1: "14px",
+                subtitle1: "0.6875rem",
+            },
+            borderRadius: {
+                lg: "30px",
+            },
+            spacing: {
+                buttonPaddingY: "8px",
+                buttonPaddingX: "32px",
+            },
+            fontWeight: {
+                bold: 700,
+            },
+        },
     },
     plugins: [],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3333, // 원하는 포트 번호 설정 (디폴트는 5173)
     open: true, // 서버 실행 시 브라우저 자동 열기 (옵션)
     strictPort: true // 포트가 사용 중이면 에러 발생 (기본값: false)
-  }
+  },
 })
