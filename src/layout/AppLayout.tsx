ce663882a74ab0e243988d686f0a75345357575b
MayHyeyeonKim <hkim7963@gmail.com>
2025-03-18 21:12:04 -0700

    finally

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -2,12 +2,12 @@ import { Outlet } from "react-router";
 
 const AppLayout = () => {
   return (
-    <div className="flex flex-row p-2 h-screen">
+    <div className="flex flex-row p-2 h-screen bg-primary">
       {/* Sidebar */}
       <div className="hidden sm:!flex flex-col w-1/3 text-red-500 pr-2 text-4xl">
         Sidebar
-        <div>Item1</div>
-        <div>Item2</div>
+        <div className=" text-green-500 bg-background p-4">Item1</div>
+        <div className="bg-accent text-yellow-500 p-4">Item2</div>
       </div>
 
       {/* Main Content */}
