7ff6ee85f69c9c2e50877448705ff9377c49a5b2
MayHyeyeonKim <hkim7963@gmail.com>
2025-04-03 01:32:10 -0700

    added playlists

--- src/layout/components/YourLibrary.tsx
+++ src/layout/components/YourLibrary.tsx
@@ -1,7 +1,10 @@
+import useGetCurrentUserPlaylists from "../../hooks/useGetCurrentUserPlaylists";
 import EmptyPlaylist from "./EmptyPlaylist";
 import LibraryHead from "./LibraryHead";
 
 const YourLibrary = () => {
+  const { data } = useGetCurrentUserPlaylists({ limit: 10, offset: 0 });
+  console.log("ddd", data);
   return (
     <>
       <LibraryHead />
